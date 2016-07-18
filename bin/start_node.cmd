c859d0dcdcbdb2c33372bfc6534fcad68083a670
lucbories <luc.bories@gmail.com>
2016-07-18 16:18:21 +0200

    fix:npm start with start_node of devapt 1.0.2

---
+++ bin/start_node.cmd
@@ -0,0 +1,5 @@
+rem SET current_dir=%CD%
+
+cd node_modules/devapt
+
+node dist\server\start_node.js %*
--- start_node.cmd
+++
@@ -1,8 +0,0 @@
-SET current_dir=%CD%
-
-SET OPENSHIFT_BINDINGS=-s NodeA:NodeALocal8080:%OPENSHIFT_NODEJS_OP%:%OPENSHIFT_NODEJS_PORT%
-
-
-cd node_modules/devapt
-
-node dist\server\start_node.js %* -t %current_dir%\dist\resources\world -b %current_dir%\node_modules\devapt-devtools\dist %OPENSHIFT_BINDINGS%
